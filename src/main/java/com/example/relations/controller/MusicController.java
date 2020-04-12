c261020be23c96010c280a3a6ca4c9a3e3286b81
Wei Wang <wei.wang@postcode-lotterie.de>
2020-04-12 18:05:06 +0200

    three entities two table share with one id

--- src/main/java/com/example/relations/controller/MusicController.java
+++ src/main/java/com/example/relations/controller/MusicController.java
@@ -1,9 +1,11 @@
 package com.example.relations.controller;
 
 
+import com.example.relations.repository.MusicCdRepository;
 import com.example.relations.repository.MusicEntityRepository;
 import com.example.relations.repository.MusicRepository;
 import com.example.relations.twoEntitiesOneTable.model.Music;
+import com.example.relations.twoEntitiesOneTable.model.MusicCd;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MusicController {
     private final MusicEntityRepository musicEntiryRepository;
     private final MusicRepository musicRepository;
+    private final MusicCdRepository musicCdRepository;
 
     @GetMapping( value = "/music/addMusic" )
     private void addMusic() {
@@ -28,9 +31,13 @@ public class MusicController {
         Music musicEntir = Music.builder().author("MusicEntity Wang").build();
 
 
+        MusicCd musicCd = MusicCd.builder().author("music cd Wang").build();
+
         musicRepository.save(music);
 
         musicEntiryRepository.save( musicEntir);
+
+        musicCdRepository.save(musicCd);
 
     }
 
---
+++ src/main/java/com/example/relations/twoEntitiesOneTable/model/MusicCd.java
@@ -0,0 +1,30 @@
+package com.example.relations.twoEntitiesOneTable.model;
+
+import com.example.relations.twoEntitiesOneTable.BaseMusic;
+import lombok.Builder;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @author Wei
+ * @date 12.04.20
+ */
+
+
+@Entity(name = "MusicCd")
+@Table(name = "music_cd")
+@Data
+@Builder
+public class MusicCd extends BaseMusic<MusicCd> {
+
+
+    @Id
+    @GeneratedValue
+    private Long id;// this id will share the same sequence with other two siblings.
+
+    private String author;
+}
